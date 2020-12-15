1ebd20a53d06849397b05d2f752f2fe43d79a894
BostonWafro <wafrogaming@gmail.com>
2020-12-15 16:52:09 +0000

    definitely finished

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ const button = document.getElementById("roll");
 const player = document.getElementById("player");
 
 button.addEventListener("click", () => {
-    button.innerHTML = "roll";
-    player.innerHTML = "Player 1";
+    if (button.innerHTML == "Play Again?") {
+        button.innerHTML = "roll";
+        player.innerHTML = "Player 1";
+        score.innerHTML = 0;
+    }
     let x = Math.floor(Math.random() * 6) + 1;
     let y = parseInt(score.innerHTML);
     dice.src = `./img/dice${x}.png`;
     
     if (x === 1) {
         player.innerHTML = "You Lose!";
-        score.innerHTML = 0;
         button.innerHTML = "Play Again?";
     }else if (score.innerHTML < 20) {
         y += x;
         score.innerHTML = y
     }
-    if (score.innerHTML >= 20) {
+    if (y >= 20) {
         player.innerHTML = "You Win!";
         button.innerHTML = "Play Again?";
     }
